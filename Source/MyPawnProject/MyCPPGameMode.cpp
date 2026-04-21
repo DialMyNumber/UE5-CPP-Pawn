a8ba3978ee04f2ea29af38801af316273147b0da
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-04-21 15:39:14 +0900

    MyCPPGameMode 클래스 생성

---
+++ Source/MyPawnProject/MyCPPGameMode.cpp
@@ -0,0 +1,2 @@
+#include "MyCPPGameMode.h"
+
---
+++ Source/MyPawnProject/MyCPPGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MyCPPGameMode.generated.h"
+
+UCLASS()
+class MYPAWNPROJECT_API AMyCPPGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
